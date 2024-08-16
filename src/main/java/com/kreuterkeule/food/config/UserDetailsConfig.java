ed200734e8e4db474569ffa3c03f225a95c32123
Kreuterkeule <moritz.siefke@protonmail.com>
2024-08-16 20:19:45 +0200

    using properties password

--- src/main/java/com/kreuterkeule/food/config/UserDetailsConfig.java
+++ src/main/java/com/kreuterkeule/food/config/UserDetailsConfig.java
@@ -1,6 +1,7 @@
 package com.kreuterkeule.food.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
@@ -19,12 +20,17 @@ public class UserDetailsConfig {
     @Autowired
     private DataSource dataSource;
 
+    @Value("${spring.security.user.name}")
+    private String default_username;
+    @Value("${spring.security.user.password}")
+    private String default_password;
+
     @Bean
     UserDetailsManager users(DataSource dataSource) {
         System.out.println();
         UserDetails defaultUser = User.builder()
-                .username("admin")
-                .password(this.passwordEncoder().encode("admin"))
+                .username(this.default_username)
+                .password(this.passwordEncoder().encode(this.default_password))
                 .roles("ADMIN", "USER")
                 .build();
         JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
